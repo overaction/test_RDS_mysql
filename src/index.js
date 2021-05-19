3f38d1e05480d83d4c6afa599cc70215c40777ce
mckim <kmc04@naver.com>
2021-05-19 13:00:57 +0900

    initial

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import http from "http";
+import mysql from "mysql";
+
+const db = mysql.createConnection({
+  host: "database-2.cf2u0cxbe2r4.ap-northeast-2.rds.amazonaws.com",
+  user: "admin",
+  password: "sik06256!",
+  port: "3306",
+  database: "node_simple_server",
+});
+
+db.connect(err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Database is connected");
+});
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+
+  db.query("show databases;", (err, result, fields) => {
+    let data = {};
+    if (err) {
+      data = { err };
+    } else if (result) {
+      data = { result };
+    }
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(
+      JSON.stringify({
+        result: "This is a simple-node-server",
+        url: req.url,
+        data,
+      }),
+    );
+    res.end();
+  });
+});
+
+server.listen(3000, () => console.log("Server is started on 3000"));
